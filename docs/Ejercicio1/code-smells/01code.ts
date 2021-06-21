f7f530826830b8f80739f4a9abf7a51bf0f276e7
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-21 11:36:33 +0200

    smell

--- docs/Ejercicio1/code-smells/01code.ts
+++ docs/Ejercicio1/code-smells/01code.ts
@@ -2,10 +2,48 @@
  * * Long Method
  */
 
-class Main {
-  showChapter(chapter) {
+/**
+ * * Code without Smell Long Method
+ */
 
+export class DailyBonusPointsCalculator {
+  PREMIUM_USER_POINTS_PER_DAY = 20;
+  NORMAL_USER_POINTS_PER_DAY = 10;
+  PREMIUM_USER_WEEKS_MULTIPLIER = 1.5;
+
+  calculate(consecutiveDays, isPremium = false) {
+    return consecutiveDays * this.NORMAL_USER_POINTS_PER_DAY;
   }
 }
 
-const a = new Main();
+/**
+ * * Code with Smell Long Method
+ */
+
+export class DailyBonusPointsCalculator2 {
+  PREMIUM_USER_POINTS_PER_DAY = 20;
+  NORMAL_USER_POINTS_PER_DAY = 10;
+  PREMIUM_USER_WEEKS_MULTIPLIER = 1.5;
+
+  calculate(consecutiveDays, isPremium = false) {
+    if (isPremium) {
+      const consecutiveWeeks = Math.floor(consecutiveDays / 7);
+
+      let consecutiveWeeksMultiplier = 1;
+      if (consecutiveWeeks > 0) {
+        consecutiveWeeksMultiplier = Math.pow(
+          this.PREMIUM_USER_WEEKS_MULTIPLIER,
+          consecutiveWeeks
+        );
+      }
+
+      return (
+        consecutiveDays *
+        this.PREMIUM_USER_POINTS_PER_DAY *
+        consecutiveWeeksMultiplier
+      );
+    } else {
+      return consecutiveDays * this.NORMAL_USER_POINTS_PER_DAY;
+    }
+  }
+}
